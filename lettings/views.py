20f8c732154c5ebd4213ba7149a6c7dd1cf8bbba
nefalis <nefalis@gmail.com>
2024-10-15 15:29:32 +0200

    rename lettings_index and profiles_index to index

--- lettings/views.py
+++ lettings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Letting
 
 
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings/lettings_index.html', context)
