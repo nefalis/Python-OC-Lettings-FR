4abb21493fcf893db3f5dc10a480e5a2348eb28e
nefalis <nefalis@gmail.com>
2024-11-05 15:33:38 +0100

    update allowed_host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'python-oc-lettings-fr-sry9.onrender.com']
 
 
 # Application definition
