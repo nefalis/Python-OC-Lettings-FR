216930589cd9389113ae6239a1cca0334d50a5b6
nefalis <nefalis@gmail.com>
2024-11-13 11:20:19 +0100

    test static file

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'python-oc-lettings-fr-sry9.onrender.com']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,12 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
 from . import views
 from .views import handler_404, handler_500
 
-
 handler404 = handler_404
 handler500 = handler_500
-
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,3 +15,10 @@ urlpatterns = [
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
